2de6721b2444496643914bfce8c85321c784c7d0
unknown <matthewaching@gmail.com>
2025-02-13 14:24:39 -0600

    Perform initial TS conversion

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,20 +1,22 @@
 import { useActionState } from 'react';
 
-const calculateSquareOfSum = async (prevState, formData) => {
-    const number1 = formData['first-number'];
-    const number2 = formData['second-number'];
+const calculateSquareOfSum = async (_: number | null, formData: FormData) => {
+    const number1 = formData.get('first-number');
+    const number2 = formData.get('second-number');
 
     await new Promise(res => setTimeout(res, 1000));
 
     if (number1 && number2) {
-        const sum = number1 + number2;
+        const sum = +number1 + +number2;
         const square = sum * sum;
         return square;
+    } else {
+        return null;
     }
 };
 
 const Calculator = () => {
-    const [answer, calculateAction, isLoading] = useActionState(calculateSquareOfSum, null);
+    const [answer, calculateAction, isLoading] = useActionState<number | null, FormData>(calculateSquareOfSum, null);
 
     return (
         <div>
